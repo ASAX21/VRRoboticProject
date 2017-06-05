7eba6996ca051cccdc44858060230f703ea983e8
joelfrewy <joelfrewin@gmail.com>
2017-06-05 15:51:03 +0800

    made very rough shell run command
    terrible hard-coded test, just to show how to call shell commands within unity

---
+++ Assets/Scripts/testTerminal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class testTerminal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ProcessStartInfo proc = new ProcessStartInfo();
+		proc.FileName = "open";
+		proc.WorkingDirectory = "/Users/JoelFrewin/Desktop";
+		proc.Arguments = "./file";
+		Process.Start(proc);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
